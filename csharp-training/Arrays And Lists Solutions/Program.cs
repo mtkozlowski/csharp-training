fb16d97c0b42086da02d130a7182c3f93fb78a3d
mtkozlowski <kooziu@gmail.com>
2017-08-24 11:42:42 +0200

    Arrays_And_Lists_Solutions & Loop_Solutions Done, Conditional WIP

--- csharp-training/Arrays And Lists Solutions/Program.cs
+++ csharp-training/Arrays And Lists Solutions/Program.cs
@@ -3,9 +3,125 @@ using System.Collections.Generic;
 
 namespace Arrays_And_Lists_Solutions
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
+        {
+            //Exercise1();
+            //Exercise2();
+            //Exercise3();
+            //Exercise4();
+            Exercise5();
+        }
+
+        private static void Exercise5()
+        {
+            //5 - Write a program and ask the user to supply a list of comma separated numbers(e.g 5, 1, 9, 2, 10). 
+            //If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
+            //otherwise, display the 3 smallest numbers in the list.
+            Console.WriteLine("please enter a list of at least 5 comma separated numbers(e.g 5, 1, 9, 2, 10).");
+            var numbersList = new List<int>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var inputElements = input.Split(',');
+
+                if (!string.IsNullOrWhiteSpace(input) && inputElements.Length >= 5)
+                {
+                    for (var i = 0; i < inputElements.Length; i++)
+                    {
+                        if (int.TryParse(inputElements[i], out int number))
+                            numbersList.Add(number);
+                    }
+                    if (numbersList.Count == inputElements.Length)
+                        break;
+                }
+                Console.WriteLine("Your list is invalid, please write it again: ");
+            }
+
+            numbersList.Sort();
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numbersList[i]);
+            }
+
+        }
+
+        private static void Exercise4()
+        {
+            //4 - Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
+            //    The list of numbers may include duplicates.Display the unique numbers that the user has entered.
+            var numbersList = new List<int>();
+            Console.WriteLine("Give as many numbers you want or type QUIT to display the unique numbers");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input?.ToLower() == "quit")
+                    break;
+
+                numbersList.Add(Convert.ToInt32(input));
+            }
+            var uniqueNumbers = new List<int>();
+            foreach (var i in numbersList)
+            {
+                if (!uniqueNumbers.Contains(i))
+                    uniqueNumbers.Add(i);
+            }
+
+            Console.WriteLine("Your unique numbers: ");
+            foreach (var uniqueNumber in uniqueNumbers)
+            {
+                Console.Write("{0} ", uniqueNumber);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Exercise3()
+        {
+            //3 - Write a program and ask the user to enter 5 numbers.If a number has been previously entered,
+            //display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, 
+            //sort them and display the result on the console.
+
+            Console.WriteLine("Give 5 different numbers: ");
+            var numbersList = new List<int>();
+
+            while (numbersList.Count != 5)
+            {
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (numbersList.Contains(number))
+                {
+                    Console.WriteLine("This number was already given, please type another one: ");
+                    continue;
+                }
+                numbersList.Add(number);
+            }
+            numbersList.Sort();
+
+            Console.WriteLine("Your numbers in a sorted order:");
+            foreach (var arg0 in numbersList)
+                Console.Write("{0} ", arg0);
+        }
+
+        private static void Exercise2()
+        {
+            Console.WriteLine("Please, give us your name:");
+            var nameInput = Console.ReadLine();
+            if (nameInput != null)
+            {
+                var nameArray = new char[nameInput.Length];
+                for (var i = nameArray.Length - 1; i >= 0; i--)
+                {
+                    nameArray[nameInput.Length - i - 1] = nameInput[i];
+                }
+
+                Console.WriteLine(nameArray);
+            }
+        }
+
+        private static void Exercise1()
         {
             var friends = new List<string>();
 
--- csharp-training/Loop Solutions/Program.cs
+++ csharp-training/Loop Solutions/Program.cs
@@ -5,20 +5,78 @@ namespace Loop_Solutions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // 1 - Write a program to count how many numbers between 1 and 100 
-            // are divisible by 3 with no remainder.Display the count on the console.
+            //Exercise1();
+            //Exercise2();
+            //Exercise3();
+            //Exercise4();
+            Exercise5();
 
-            var dividable = new List<int>();
-            var count = 0;
-            for (int i = 1; i <= 100; i++)
+            Console.ReadKey();
+        }
+
+        private static void Exercise5()
+        {
+            //5 - Write a program and ask the user to enter a series of numbers separated by comma.
+            //    Find the maximum of the numbers and display it on the console. 
+            //    For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+            Console.WriteLine("Enter a series of numbers separated by comma.");
+            var input = Console.ReadLine().Split(',');
+            var numbersArray = new int[input.Length];
+            var maxNumber = 0;
+            for (var i = 0; i < numbersArray.Length; i++)
             {
-                var i1 = i % 3 == 0 ? ++count : count;
+                if (int.TryParse(input[i], out int number))
+                    numbersArray[i] = number;
+
+                if (maxNumber < number)
+                    maxNumber = number;
             }
 
-            Console.WriteLine("Total count of these numbers is: " + count);
+            Console.WriteLine("The highest number is {0}", maxNumber);
+        }
 
+        private static void Exercise4()
+        {
+            //4 - Write a program that picks a random number between 1 and 10.Give the user 4 chances to guess the number.
+            //    If the user guesses the number, display “You won"; otherwise, display “You lost".
+            //    (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+            var randomNumber = new Random().Next(1, 10);
+            var guessCount = 4;
+            Console.WriteLine("Guess a number, you have 4 guesses");
+            while (guessCount != 0)
+            {
+                var guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("Congratulations! You did it!");
+                    break;
+                }
+                guessCount--;
+                Console.WriteLine("Sorry, you missed that! Only {0} chance(s) left.", guessCount);
+            }
+        }
+
+        private static void Exercise3()
+        {
+            // 3- Write a program and ask the user to enter a number. 
+            // Compute the factorial of the number and print it on the console.
+            // For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+            Console.WriteLine("Give me an input for factorial: ");
+            var factorial = Convert.ToInt32(Console.ReadLine());
+            int factorialResult = 1;
+            while (factorial != 0)
+            {
+                factorialResult *= factorial;
+                factorial--;
+            }
+            Console.WriteLine("Factorial: " + factorialResult);
+        }
+
+        private static void Exercise2()
+        {
             // 2 - Write a program and continuously ask the user to enter a number or "ok" to exit.
             // Calculate the sum of all the previously entered numbers and display it on the console.
 
@@ -36,21 +94,21 @@ namespace Loop_Solutions
             }
 
             Console.WriteLine("sumOfInputs: " + sumOfInputs);
+        }
 
-            // 3- Write a program and ask the user to enter a number. 
-            // Compute the factorial of the number and print it on the console.
-            // For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
-            Console.WriteLine("Give me an input for factorial: ");
-            var factorial = Convert.ToInt32(Console.ReadLine());
-            int factorialResult = 1;
-            while (factorial != 0)
+        private static void Exercise1()
+        {
+            // 1 - Write a program to count how many numbers between 1 and 100 
+            // are divisible by 3 with no remainder.Display the count on the console.
+
+            var dividable = new List<int>();
+            var count = 0;
+            for (int i = 1; i <= 100; i++)
             {
-                factorialResult *= factorial;
-                factorial--;
+                var i1 = i % 3 == 0 ? ++count : count;
             }
-            Console.WriteLine("Factorial: " + factorialResult);
 
-            Console.ReadKey();
+            Console.WriteLine("Total count of these numbers is: " + count);
         }
     }
 }
